c1df61db7ac798ef6fa5572c79f5636476e0e204
vocterf <vocterr07@gmail.com>
2026-05-23 22:37:28 +0200

    Basic CPU setup

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,44 @@
 
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq)]
 pub struct CPU {
-    v: [u8; 16],
-    i: u16,
+    registers: Registers,
     pc: u16,
-    sp: u8,
-    stack: [u16; 16],
-    memory: [u8; 4096]
+    sp: u16,
+    memory: [u8; 0x10000]
 }
 
 impl CPU {
-    fn new() -> Self {
+    pub fn new() -> Self {
         CPU {
-            v: [0; 16],
-            i: 0,
-            pc: 0x200,
-            sp: 0,
-            stack: [0; 16],
-            memory: [0; 4096]
+            registers: Registers::new(),
+            pc: 0x0000,
+            sp: 0x0000,
+            memory: [0; 0x10000]
         }
     }
 }
 
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct Registers {
+    pub a: u8,
+    pub f: u8,
+    pub b: u8,
+    pub c: u8,
+    pub d: u8,
+    pub e: u8,
+    pub h: u8,
+    pub l: u8,
+}
+
+impl Registers {
+    pub fn new() -> Self {
+        Registers { a: 0x00, f: 0x00, b: 0x00, c: 0x00, d: 0x00, e: 0x00, h: 0x00, l: 0x00 }
+    }
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let cpu = CPU::new();
 }
